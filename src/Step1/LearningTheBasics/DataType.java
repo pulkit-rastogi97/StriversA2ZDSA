90845e0546b18b2f3ecc677754d0ccb6839073cd
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2024-04-19 09:21:56 +0530

    Solved 2nd question: Data Types size

---
+++ src/Step1/LearningTheBasics/DataType.java
@@ -0,0 +1,31 @@
+package Step1.LearningTheBasics;
+
+import java.util.Scanner;
+
+public class DataType {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String dataType = scan.next();
+		int size = dataTypes(dataType);
+		System.out.println(size);
+		scan.close();
+	}
+	
+	private static int dataTypes(String type) {
+        
+        if(type.equalsIgnoreCase("Long")){
+            return 8;
+        }else if(type.equalsIgnoreCase("Integer")){
+            return 4;
+        }else if(type.equalsIgnoreCase("Float")){
+            return 4;
+        }else if(type.equalsIgnoreCase("Double")){
+            return 8;
+        }else if(type.equalsIgnoreCase("Character")){
+            return 1;
+        }
+
+        return 4;  // default for 32-bit memory pointer
+    }
+
+}
