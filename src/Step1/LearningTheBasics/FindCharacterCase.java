2654942c5aceb1a54b9a70ed04b904271c85ac82
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2024-04-19 09:21:31 +0530

    Solved 1st question: Find Character case.

---
+++ src/Step1/LearningTheBasics/FindCharacterCase.java
@@ -0,0 +1,28 @@
+package Step1.LearningTheBasics;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * Question Link:
+ * https://www.naukri.com/code360/problems/find-character-case_58513?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+ * 
+ * @author pulkit-rastogi97
+ *
+ */
+public class FindCharacterCase {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char ch = scan.next().charAt(0);
+
+        if(ch >= 'A' && ch <= 'Z'){
+            System.out.println(1);
+        }else if(ch >= 'a' && ch <= 'z'){
+            System.out.println(0);
+        }else{
+            System.out.println(-1);
+        }
+        
+        scan.close();
+    }
+}
