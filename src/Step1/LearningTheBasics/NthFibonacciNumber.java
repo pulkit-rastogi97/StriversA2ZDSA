b8414bc5c01b35526b054d10f3ff0c5b3ed637dd
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2024-04-19 19:50:22 +0530

    Solved Nth Fibonacci Number

---
+++ src/Step1/LearningTheBasics/NthFibonacciNumber.java
@@ -0,0 +1,23 @@
+package Step1.LearningTheBasics;
+
+/**
+ * Question Link: https://www.naukri.com/code360/problems/nth-fibonacci-number_74156?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+ * 
+ * @author pulkit-rastogi97
+ *
+ */
+public class NthFibonacciNumber {
+	public static void main(String[] args) {		
+		int n = IOUtils.takeIntInput();
+
+		int a = 0;
+		int b = 1;
+		int c = 1;
+		for (int i = 1; i < n; i++) {
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		System.out.println(c);
+	}
+}
